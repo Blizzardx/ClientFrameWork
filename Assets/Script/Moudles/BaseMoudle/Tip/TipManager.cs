375ee5cb20de06eef12e10f418d8a483875e0fb6
Bao Xue <Blizzard_xc@126.com>
2015-08-16 02:30:29 +0800

    add tip system

---
+++ Assets/Script/Moudles/BaseMoudle/Tip/TipManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class TipManager : Singleton<TipManager>
+{
+    #region alert
+    public void Alert(string content)
+    {
+        
+    }
+    public void Alert(string title, string content)
+    {
+        
+    }
+    public void Alert(string title, string content, string correct, Action<bool> callBack)
+    {
+        
+    }
+    public void Alert(string title, string content, string correct, string cancle, Action<bool> callBack)
+    {
+
+    }
+    #endregion
+
+
+    #region common text tip
+    public void Tip(string content)
+    {
+        
+    }
+    #endregion
+
+    #region tip
+
+    #endregion
+}
