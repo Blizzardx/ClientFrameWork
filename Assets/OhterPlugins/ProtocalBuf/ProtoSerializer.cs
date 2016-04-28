84229e5066dcbc1b6cf93d88682ee977f845ad76
blizzard_xc@126.com <blizzard_xc@126.com>
2016-04-28 15:44:56 +0800

    add protocal buf

---
+++ Assets/OhterPlugins/ProtocalBuf/ProtoSerializer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using System.IO;
+
+public class ProtoSerializer
+{
+    public static byte[] Serialize<T>(T t)
+    {
+        using (MemoryStream ms = new MemoryStream())
+        {
+            ProtoBuf.Serializer.Serialize(ms, t);
+            byte[] data = new byte[ms.Length];
+            ms.Position = 0;
+            ms.Read(data, 0, data.Length);
+            return data;
+        }
+    }
+
+    public static T DeSerialize<T>(byte[] content)
+    {
+        using (MemoryStream ms = new MemoryStream(content))
+        {
+            T t = ProtoBuf.Serializer.Deserialize<T>(ms);
+            return t;
+        }
+    }
+}
