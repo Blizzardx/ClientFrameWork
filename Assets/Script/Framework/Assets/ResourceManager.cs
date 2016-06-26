4787eff88c9209cc02d5569918d7055665ccb2f7
Bao Xue <Blizzard_xc@126.com>
2016-06-26 17:49:14 +0800

    update asset manager

---
+++ Assets/Script/Framework/Assets/ResourceManager.cs
@@ -0,0 +1,12 @@
+﻿using Common.Tool;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Framework.Asset
+{
+    public class ResourceManager : MonoSingleton<ResourceManager>
+    {
+
+
+    }
+}
